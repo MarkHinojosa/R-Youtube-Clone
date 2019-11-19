25e06b1ddaa9e69473fced9765a428187932aedf
mark <markhinojosa1212@gmail.com>
2019-11-18 18:16:13 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+import youtube from "./api/youtube";
+import { SearchBar, VideoList, VideoDetail } from "./components";
 
 export default class App extends Component {
   render() {
-    return <div>This is the APP component</div>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <SearchBar />
+            </Grid>
+            <Grid item xs={12}>
+              <VideoDetail />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
