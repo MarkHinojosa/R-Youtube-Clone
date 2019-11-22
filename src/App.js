97473ca1fb7fed3bf58b80273c52074706f89409
mark <markhinojosa1212@gmail.com>
2019-11-21 18:17:09 -0600

    predloading search keywords

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   };
 
   componentDidMount = () => {
-    this.handleSubmit("eunice new mexico rollercoaster road");
+    this.handleSubmit("react.js");
   };
   handleSubmit = async searchTerm => {
     let response = await youtube.get("search", {
